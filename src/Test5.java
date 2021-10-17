dd9a406b49b816c854fdf22387bff4979bd9ba27
msuhagiya <suhagiya793@gmail.com>
2021-10-17 12:08:12 +0100

    I like git five times

---
+++ src/Test5.java
@@ -0,0 +1,9 @@
+public class Test5 {
+    public static void main(String[] args) {
+        System.out.println("I like Git");
+        System.out.println("I like Git");
+        System.out.println("I like Git");
+        System.out.println("I like Git");
+        System.out.println("I like Git");
+    }
+}
